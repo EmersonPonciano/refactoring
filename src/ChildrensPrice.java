14e6bef85986e33f7eb69aaaed024ff7c075cf37
Emerson Ponciano <emersonponciano2@gmail.com>
2022-12-13 12:32:03 -0300

    Commit 7

--- src/ChildrensPrice.java
+++ src/ChildrensPrice.java
@@ -5,7 +5,7 @@ public class ChildrensPrice extends Price {
     public double getCharge(int daysRented) {
         double result = 1.5;
         if (daysRented > 3)
-            result += (daysRented - 3) * 1.5
+            result += (daysRented - 3) * 1.5;
         return result;
     }
 }
